5f7e114e5e4073513fade69d5a1c7bd57c65d53f
Arcanis <arcaniscz@gmail.com>
2014-08-31 22:07:06 +0200

    no message

--- app/view/Content/ContentView.js
+++ app/view/Content/ContentView.js
@@ -1,7 +1,7 @@
 define([
     "text!./ContentView.html",
     "less!./ContentView.less",
-    "directive!app/component/countries/CountryFlag"
+    "directive!app/component/countries/CountryFlag AS flag"
 ], function (template) {
     return {
         default: true,
